99d515e916af3f5f874ae1a8ae8e77a9a66524b3
s230392 <s230392@greenhead.ac.uk>
2024-06-07 14:39:49 +0100

    Updated input validation Added default value to Hours and Minutes

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ const addEmployeeClick = () => {
     const li = document.createElement("li");
 
 }
-const handleNumOnly = (id) => {
+const validateNumberInput = (id) => {
     // get the element
     const input = document.getElementById(id);
     let inputtedValue = input.value;
@@ -13,5 +13,18 @@ const handleNumOnly = (id) => {
     if(!/^[0-9]+$/g.test(inputtedValue)) {
         // remove the invalid characters if they are present
         input.value = inputtedValue.replace(/\D/g, "");
+    }
+    // stop leading zeros from filling up the space
+    input.value = String(Number(input.value));
+    // add an upper limit to the inputs
+    const numericValue = Number(input.value);
+    // no need to check if this is a real number as this has already been validated above
+    if(id === "employeeHours" && numericValue > 99){
+        input.value = input.value.substring(0, input.value.length - 1);
+        window.alert("Max number of Hours is 99")
+    }
+    if(id === "employeeMinutes" && numericValue > 59){
+        input.value = input.value.substring(0, input.value.length - 1);
+        window.alert("Max number of Minutes is 59")
     }
 }
