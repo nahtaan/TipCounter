2d7581293b7ecfdf624e01a269ebea9a8b9e43ab
Nathan Bateson <nathanjohnbateson@gmail.com>
2024-06-10 23:03:39 +0100

    Final fix for entry list display

--- src/script.js
+++ src/script.js
@@ -29,20 +29,20 @@ const addEmployeeClick = () => {
         return;
     }
     const newItem = fromHTML(`
-    <div class="flex flex-row place-content-center bg-neutral-700 rounded-xl divide-x-2 p-3 divide-neutral-400 max-w-full">
-        <div class="flex justify-center items-center align-middle">
-            <svg class="fill-true-red lg:h-9 lg:w-7 lg:pt-2 h-7 w-7 justify-self-center place-self-center" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 24 24">
-                <path d="M 10.806641 2 C 10.289641 2 9.7956875 2.2043125 9.4296875 2.5703125 L 9 3 L 4 3 A 1.0001 1.0001 0 1 0 4 5 L 20 5 A 1.0001 1.0001 0 1 0 20 3 L 15 3 L 14.570312 2.5703125 C 14.205312 2.2043125 13.710359 2 13.193359 2 L 10.806641 2 z M 4.3652344 7 L 5.8925781 20.263672 C 6.0245781 21.253672 6.877 22 7.875 22 L 16.123047 22 C 17.121047 22 17.974422 21.254859 18.107422 20.255859 L 19.634766 7 L 4.3652344 7 z"></path>
-            </svg>
-        </div>
-        <div class="pl-2 pr-2 lg:pr-0 lg:pl-0 block text-clip overflow-hidden">
-            <p class="text-ellipsis break-words">${nameEntry}</p>
-        </div>
-        <div class="flex lg:flex-col sm:flex-row gap-3 pl-2 pr-2 lg:pr-2">
-            ${hoursFormat}
-            ${minutesFormat}
-        </div>
+    <div class="flex flex-row place-content-center bg-neutral-700 rounded-xl divide-x-2 p-3 divide-neutral-400 max-w-full min-w-0">
+    <div class="flex justify-center items-center align-middle">
+        <svg class="fill-true-red h-7 w-7 justify-self-center place-self-center" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 24 24">
+            <path d="M 10.806641 2 C 10.289641 2 9.7956875 2.2043125 9.4296875 2.5703125 L 9 3 L 4 3 A 1.0001 1.0001 0 1 0 4 5 L 20 5 A 1.0001 1.0001 0 1 0 20 3 L 15 3 L 14.570312 2.5703125 C 14.205312 2.2043125 13.710359 2 13.193359 2 L 10.806641 2 z M 4.3652344 7 L 5.8925781 20.263672 C 6.0245781 21.253672 6.877 22 7.875 22 L 16.123047 22 C 17.121047 22 17.974422 21.254859 18.107422 20.255859 L 19.634766 7 L 4.3652344 7 z"></path>
+        </svg>
     </div>
+    <div class="pl-2 pr-2 block text-clip overflow-hidden">
+        <p class="text-ellipsis break-words">${nameEntry}</p>
+    </div>
+    <div class="flex flex-row gap-3 pl-2 pr-2">
+        ${hoursFormat}
+        ${minutesFormat}
+    </div>
+</div>
     `);
     
     // append the overall container to the entrylist container
